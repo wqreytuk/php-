0c07d42501e7a53e1c67e38a2538f6f82b5399e9
wqreytuk <48377190+wqreytuk@users.noreply.github.com>
2019-03-21 18:36:17 +0800

    Create index.php

---
+++ 简易记事本系统/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>简易记事本（压缩版）</title>
+</head>
+<frameset border="0" rows="130,*">
+<frame src="list.php" name="up" scrolling="no" />
+<frame src="blank.html" name="down" scrolling="auto" />
+</frameset>
+</html>
